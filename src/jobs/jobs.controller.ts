8b9f7d88e57549f427af7c36b41f016e2f797ba6
Samenodin Sarip <samenodinsarip31@gmail.com>
2025-01-09 22:46:12 +0800

    Implemented scraping and summarizing in job controller

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -4,38 +4,74 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Logger,
   Param,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+const { BAD_REQUEST, NOT_FOUND } = HttpStatus;
 import mongoose from 'mongoose';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/CreateJob.dto';
 import { JobStatuses } from './jobs.types';
 import { Messages } from 'src/constants';
+import { ScraperService } from 'src/providers/scraper/scraper/scraper.service';
+import { LlmService } from 'src/providers/llm/llm/llm.service';
 
 @Controller('jobs')
 export class JobsController {
-  constructor(private readonly jobsService: JobsService) {}
+  private logger: Logger;
+  constructor(
+    private readonly jobsService: JobsService,
+    private readonly scraperService: ScraperService,
+    private readonly llmService: LlmService,
+  ) {
+    this.logger = new Logger(JobsController.name);
+  }
 
   @Get(':id')
   async getJobById(@Param('id') id: string) {
     const is_valid = mongoose.Types.ObjectId.isValid(id);
-    if (!is_valid)
-      throw new HttpException(Messages.INVALID_ID, HttpStatus.BAD_REQUEST);
+    if (!is_valid) throw new HttpException(Messages.INVALID_ID, BAD_REQUEST);
     const result = await this.jobsService.getJobById(id);
-    if (!result)
-      throw new HttpException(Messages.JOB_NOT_FOUND, HttpStatus.NOT_FOUND);
+    if (!result) throw new HttpException(Messages.JOB_NOT_FOUND, NOT_FOUND);
     return result;
   }
 
   @Post()
   @UsePipes(new ValidationPipe())
-  createJob(@Body() params: CreateJobDto) {
+  async createJob(@Body() params: CreateJobDto) {
+    const { url } = params;
+
+    let summary = '';
+    let status = JobStatuses.FAILED;
+    let error_message = '';
+
+    try {
+      const text = await this.scraperService.scrapeWebpage(url);
+
+      if (!text) {
+        error_message = Messages.NO_TEXT_CONTENT_FOUND;
+      }
+
+      const result = await this.llmService.generateSummary(text);
+      summary = result?.summary ?? '';
+      status = result?.success ? JobStatuses.COMPLETED : JobStatuses.FAILED;
+
+      if (!result?.success) {
+        error_message = result?.error_message;
+      }
+    } catch (error) {
+      error_message = error_message || error.message;
+      this.logger.error('Error in createJob:', error_message);
+    }
+
     return this.jobsService.createJob({
       ...params,
-      status: JobStatuses.PENDING,
+      summary,
+      status,
+      ...(error_message && { error_message }),
       created_date: new Date(),
     });
   }
--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -3,6 +3,8 @@ import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Job, JobSchema } from 'src/schemas/Jobs.schema';
+import { ScrapperModule } from 'src/providers/scraper/scraper/scraper.module';
+import { LlmModule } from 'src/providers/llm/llm/llm.module';
 
 @Module({
   providers: [JobsService],
@@ -14,6 +16,8 @@ import { Job, JobSchema } from 'src/schemas/Jobs.schema';
         schema: JobSchema,
       },
     ]),
+    ScrapperModule,
+    LlmModule,
   ],
 })
 export class JobsModule {}
