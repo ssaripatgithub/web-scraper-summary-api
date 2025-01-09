70a63a99ff836a637537a302dc1f73950db8c111
Samenodin Sarip <samenodinsarip31@gmail.com>
2025-01-09 21:23:38 +0800

    Added new modules in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { JobsModule } from './jobs/jobs.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScrapperModule } from './providers/scraper/scraper/scraper.module';
+import { LlmModule } from './providers/llm/llm/llm.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './validation/config/config.validate';
 const { DATABASE_CONNECTION = '' } = process.env;
 
 @Module({
-  imports: [JobsModule, MongooseModule.forRoot(DATABASE_CONNECTION)],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+    JobsModule,
+    MongooseModule.forRoot(DATABASE_CONNECTION),
+    ScrapperModule,
+    LlmModule,
+  ],
   controllers: [],
   providers: [],
 })
