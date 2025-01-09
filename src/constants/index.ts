37097a772a2ef23d381e3106a4e1d194f3827f14
Samenodin Sarip <samenodinsarip31@gmail.com>
2025-01-09 18:15:57 +0800

    Added constants

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export enum Messages {
+  INVALID_ID = 'Invalid ID',
+  JOB_NOT_FOUND = 'Job not found',
+  JOB_CREATED_SUCCESSFULLY = 'Job created successfully',
+  JOB_CREATION_FAILED = 'Failed to create job.',
+  ERROR_SAVING_JOB = 'Error saving job',
+  SOMETHING_WENT_WRONG = 'Something went wrong',
+}
